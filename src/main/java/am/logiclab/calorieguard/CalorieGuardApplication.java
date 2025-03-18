f8a222c92334740a302ccff50ee28978367c6265
Raf <raf.gasparyan.17@mail.ru>
2025-03-18 20:43:10 +0400

    Initial commit (with database integration and configuration)

---
+++ src/main/java/am/logiclab/calorieguard/CalorieGuardApplication.java
@@ -0,0 +1,13 @@
+package am.logiclab.calorieguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/logiclab/calorieguard/CalorieGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package am.logiclab.calorieguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
